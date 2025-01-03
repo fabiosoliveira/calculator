ddf51b97b0ac087b8f4f63e772e6423cd2fbd1c4
Fábio <fabio.oliveira@poscontrole.com.br>
2025-01-02 22:15:43 -0300

    ✨ feat(calculator): add command-line interface for basic arithmetic operations
    - add package `math` for arithmetic operations
    - use `os.Args` to receive command-line arguments
    - switch statement to handle different operations based on the first argument passed.
    
    This commit adds support for the `sum`, `sub`, `div`, `mult` operations.

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -1,5 +1,38 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"strconv"
+
+	"github.com/fabiosoliveira/calculator/internal/math"
+)
+
 func main() {
 
+	args := os.Args[1:]
+
+	error := "Você precisa passar os argumentos: <method: sum, sub, div, mult > <parametro1> <parametro2>"
+	if len(args) != 3 {
+		panic(error)
+	}
+
+	arg1, err1 := strconv.ParseFloat(args[1], 64)
+	arg2, err2 := strconv.ParseFloat(args[2], 64)
+	if err1 != nil || err2 != nil {
+		panic("Os parâmetros precisam ser números válidos")
+	}
+
+	switch args[0] {
+	case "sum":
+		fmt.Println(math.Add(arg1, arg2))
+	case "sub":
+		fmt.Println(math.Subtract(arg1, arg2))
+	case "div":
+		fmt.Println(math.Divide(arg1, arg2))
+	case "mult":
+		fmt.Println(math.Multiply(arg1, arg2))
+	default:
+		panic(error)
+	}
 }
